34cff969d4607dd1ef4404b0d0870d8593f7ac67
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-07-15 13:35:22 +0900

    hard mode

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+import { collection } from "firebase/firestore";
 import { AES, enc } from "crypto-js";
 import * as functions from "firebase-functions";
 const hri = require("human-readable-ids").hri;
@@ -6,8 +7,17 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
+enum PlayMode {
+  EasyMode = "EasyMode",
+  HardMode = "HardMode",
+}
+
 const CRYPTO_KEY = "VeryLongAnimals";
 const CRYPTO_KEY2 = "verycorrectlength";
+
+const CRYPTO_KEY_EASY = "easy";
+const CRYPTO_KEY_HARD = "hard";
+
 export const nmgIpjNOiD = functions.https.onCall(
   async (
     data: { pkYRAkEQw5: string; PQ8rn0Twca: string; score: number },
@@ -56,38 +66,81 @@ export const createUser = functions.firestore
       );
   });
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+///////////// New APIs
+export const onCreateAuthUser = functions.auth.user().onCreate(async (user) => {
+  const name = "very-" + hri.random();
+
+  const easyModePromise = db.collection(PlayMode.EasyMode).doc(user.uid).set(
+    {
+      name: name,
+    },
+    { merge: true }
+  );
+  const hardModePromise = db.collection(PlayMode.HardMode).doc(user.uid).set(
+    {
+      name: name,
+    },
+    { merge: true }
+  );
+  const promises = [easyModePromise, hardModePromise];
+  return Promise.all(promises).catch((error) => {
+    console.error(error);
+  });
 });
 
-export const addNumbers = functions.https.onCall((data) => {
-  // [END addFunctionTrigger]
-  // [START readAddData]
-  // Numbers passed from the client.
-  const firstNumber = data.firstNumber;
-  const secondNumber = data.secondNumber;
-  // [END readAddData]
+// pkYRAkEQw5は使ってない
+export const nmgIpjNOiD2 = functions.https.onCall(
+  async (
+    data: {
+      pkYRAkEQw5: string;
+      PQ8rn0Twca: string;
+      score: number;
+      mode: PlayMode;
+    },
+    context
+  ) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        "permission-denied",
+        "permission denied"
+      );
+    }
 
-  // [START addHttpsError]
-  // Checking that attributes are present and are numbers.
-  if (!Number.isFinite(firstNumber) || !Number.isFinite(secondNumber)) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError(
-      "invalid-argument",
-      "The function must be called with " +
-        'two arguments "firstNumber" and "secondNumber" which must both be numbers.'
+    let cryptoKeyMode = "";
+    let collectionName = "";
+    if (data.mode === PlayMode.EasyMode) {
+      cryptoKeyMode = CRYPTO_KEY_EASY;
+      collectionName = PlayMode.EasyMode;
+    } else if (data.mode === PlayMode.HardMode) {
+      cryptoKeyMode = CRYPTO_KEY_HARD;
+      collectionName = PlayMode.HardMode;
+    } else {
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "invalid argument"
+      );
+    }
+
+    const decryptedScore = Number(
+      AES.decrypt(
+        data.PQ8rn0Twca,
+        CRYPTO_KEY + CRYPTO_KEY2 + cryptoKeyMode
+      ).toString(enc.Utf8)
     );
-  }
-  // [END addHttpsError]
 
-  // [START returnAddData]
-  // returning result.
-  return {
-    firstNumber: firstNumber,
-    secondNumber: secondNumber,
-    operator: "+",
-    operationResult: firstNumber + secondNumber,
-  };
-  // [END returnAddData]
-});
+    if (decryptedScore !== data.score) {
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "invalid argument"
+      );
+    }
+    await db.collection(collectionName).doc(context.auth.uid).set(
+      {
+        score: decryptedScore,
+      },
+      { merge: true }
+    );
+
+    return { score: data.score };
+  }
+);
